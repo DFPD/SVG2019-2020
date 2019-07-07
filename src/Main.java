9a080df8331c8b6b7029d3dd751e173bd2fb230f
DFPD <prairiedog809@yahoo.com>
2019-07-07 16:48:59 -0500

    Made some stuff about names, hopefully make names on top left button

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main
 {
 public static void main (String[] args)
@@ -6,8 +7,11 @@ public static void main (String[] args)
     ayy.shuffle();
     ayy.getCards();
     JohnnyGUI myGUI = new JohnnyGUI();
-System.out.print("hi");
     myGUI.setVisible(true);
+    System.out.println("Wassup doug welcome to the 24 game! Please enter your name:");
+    Scanner johnny = new Scanner(System.in);
+    String yeet = johnny.next();
+    System.out.println("W");
 }
 
 }
