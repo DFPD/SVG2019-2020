2ed2b65f8c11607a5fb756a85a2db15a19a1f275
tork13 <Tokarsky27@gmail.com>
2019-07-14 14:37:49 -0500

    changes arrays to arraylists

--- src/Deck.java
+++ src/Deck.java
@@ -1,34 +1,34 @@
+import java.util.ArrayList;
+
 public class Deck
 {
-    private Card[] cards = new Card[52]; // this is our array that reps a deck of cards
+    private ArrayList<Card> cards = new ArrayList<Card>(0); // this is our array that reps a deck of cards
 
     public Deck()
     {
-        int pointer = 0; // marks where in the array of cards we at
         for(int i = 1; i < 5; i++)
         {
             for(int j = 1; j < 14; j++)
             {
-                cards[pointer] = new Card(i , j);
-                pointer++;
+                cards.add(new Card(i , j));
             }
         }
     }
 
     public void getCards()
     {
-        for(int i = 0; i < cards.length; i++)
-            System.out.println(cards[i].getName());
+        for(int i = 0; i < cards.size(); i++)
+            System.out.println(cards.get(i).getName());
     }
 
     public void shuffle()
     {
         for (int j = 0; j < 8; j++) {
-            for (int i = 0; i < cards.length; i++) {
+            for (int i = 0; i < cards.size(); i++) {
                 int randIndex = (int) (Math.random() * 52);
-                Card temp = cards[i];
-                cards[i] = cards[randIndex];
-                cards[randIndex] = temp;
+                Card temp = cards.get(i);
+                cards.set(i,cards.get(randIndex));
+                cards.set(randIndex,temp);
 
             }
         }
