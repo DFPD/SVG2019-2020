2d672aed3f20b2f1030d11cc2370f1c32daa0ee3
DFPD <prairiedog809@yahoo.com>
2019-07-21 16:29:52 -0500

    More AI stuff made

--- src/AIclass.java
+++ src/AIclass.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
-public class AIclass
-{
+public class AIclass {
     private int timeConstraint;
     private long timer;
 
@@ -19,16 +18,19 @@ public class AIclass
         } else return false;
     }
 
-    public void calculate(ArrayList<Card> Hand) {
-        boolean works = false;
+    public String calculate(ArrayList<Card> Hand) {
+        int tempHand; // first number you take out of the hand
+        int tempHand2; // number you create after first combo (3 values left)
+        int tempHand3; // number created after second combo (2 values left)
         int firstNum;
         int secondNum;
         int thirdNum; // combines these numbers to make 24
-        while (works != true) {
-            for (int i = 0; i < 4; i++) // goes through first 4 cards
+        for (int u = 0; u < 4; u++) // goes through 4 cards in the hand
+            tempHand = Hand.get(u).getValue();
+            for (int i = 0; i < 4; i++) // uses that 1 card and combines in the 4 math opps
             {
-                Hand.get(i).getValue();
-                for (int j = 0; j < 3; j++) // operates them in addition
+                tempHand
+                for (int j = 0; j < 3; j++) // combines tempHand2 with the rest in the 4 opps
                 {
 
                     for (int k = 0; k < 2; k++) // through final two, makes 24 after combo theoretically
@@ -38,8 +40,8 @@ public class AIclass
                 }
             }
 
-            // if it works, the values are stored in variables to create an equation that makes 24
-        }
+        return "";
+        // if it works, the values are returned in a string of the equation that makes 24
     }
 
 }
